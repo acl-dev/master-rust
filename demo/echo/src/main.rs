ec3ccc900437fbffb6c6f3023b8f7307b74f692c
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2019-03-15 21:57:26 +0800

    use fn callback in tcp_start_alone/tcp_start_daemon

--- demo/echo/src/main.rs
+++ demo/echo/src/main.rs
@@ -1,7 +1,29 @@
+use std::io::{Read, Write, Error};
+use std::net::TcpStream;
 extern crate master;
+
+fn handle_echo(mut stream: TcpStream) ->Result<(), Error> {
+    println!("incoming from {}", stream.peer_addr()?);
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write(&buf[..bytes_read])?;
+    }
+}
+
+fn handle_conn(stream: TcpStream) {
+    let r = handle_echo(stream);
+    match r {
+        Ok(_v) => {}
+        Err(_e) => {}
+    }
+}
 
 fn main() {
     let addrs = "127.0.0.1:8188, 127.0.0.1:8288; 127.0.0.1:8388;".to_string();
     println!("start listen {}", addrs);
-    master::service::tcp_start_alone(&addrs);
+    master::service::tcp_start_alone(&addrs, handle_conn);
 }
--- master/src/service.rs
+++ master/src/service.rs
@@ -1,21 +1,8 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write, Error};
 use std::os::unix::io::FromRawFd;
 
-fn handle_conn(mut stream: TcpStream) ->Result<(), Error> {
-    println!("incoming from {}", stream.peer_addr()?);
-    let mut buf = [0; 512];
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 {
-            return Ok(());
-        }
-        stream.write(&buf[..bytes_read])?;
-    }
-}
-
-pub fn get_listeners_by_addrs(addrs: &Vec<String>) -> Vec<TcpListener> {
+fn get_listeners_by_addrs(addrs: &Vec<String>) -> Vec<TcpListener> {
     let mut listeners = Vec::new();
     for addr in addrs.iter() {
         let listener = TcpListener::bind(addr).expect("coudn't bind");
@@ -26,7 +13,7 @@ pub fn get_listeners_by_addrs(addrs: &Vec<String>) -> Vec<TcpListener> {
     listeners
 }
 
-pub fn get_listeners() -> Vec<TcpListener> {
+fn get_listeners() -> Vec<TcpListener> {
     let mut listeners = Vec::new();
     for fd in 6..7 {
         let listener;
@@ -39,28 +26,22 @@ pub fn get_listeners() -> Vec<TcpListener> {
     listeners
 }
 
-fn listen_by(listener: TcpListener) {
+fn listen_by(listener: TcpListener, f: fn(TcpStream)) {
     println!("Thread {:?} started! {:?}",
         thread::current().id(), listener.local_addr().unwrap());
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
-            let r = handle_conn(stream);
-            match r {
-                Ok(_v) => {}
-                Err(_e) => {}
-            }
-        });
+        thread::spawn(move || { f(stream); });
     }
 }
 
-fn start_listening(listeners: &mut Vec<TcpListener>) {
+fn start_listening(listeners: &mut Vec<TcpListener>, f: fn(TcpStream)) {
     let mut handles = Vec::new();
     loop {
         let listener = listeners.pop();
         match listener {
             Some(v) => {
-                let handle = thread::spawn(move || { listen_by(v); });
+                let handle = thread::spawn(move || { listen_by(v, f); });
                 handles.push(handle);
             }
             None => break,
@@ -76,7 +57,7 @@ fn start_listening(listeners: &mut Vec<TcpListener>) {
     }
 }
 
-pub fn tcp_start_alone(addrs: &String) {
+pub fn tcp_start_alone(addrs: &String, f: fn(TcpStream)) {
     let addrs = addrs.replace(" ", ";");
     let addrs = addrs.replace(",", ";");
     let addrs = addrs.replace("\t", ";");
@@ -90,10 +71,10 @@ pub fn tcp_start_alone(addrs: &String) {
     }
 
     let mut listeners = get_listeners_by_addrs(&addrs_v);
-    start_listening(&mut listeners);
+    start_listening(&mut listeners, f);
 }
 
-pub fn tcp_start_daemon() {
+pub fn tcp_start_daemon(f: fn(TcpStream)) {
     let mut listeners = get_listeners();
-    start_listening(&mut listeners);
+    start_listening(&mut listeners, f);
 }
